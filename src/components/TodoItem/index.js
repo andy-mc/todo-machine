07923ec616752ba5d3bbbbf1e33604ec1b9740d6
Andrés Muñoz <andres.munoz@idukay.com>
2021-09-11 12:42:15 -0500

    move logic oncomplete and delete a level above

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './Items.css'
 
-function TodoItem({todo: {_id: todo_id, text, completed}, onComplete, onDelete}) {
+function TodoItem({todo: {text, completed}, onComplete, onDelete}) {
     return (
       <li className="TodoItem">
-        <span onClick={()=>{onComplete(todo_id)}} className={`Icon Icon-check ${completed && 'Icon-check--active'}`}>
+        <span onClick={onComplete} className={`Icon Icon-check ${completed && 'Icon-check--active'}`}>
           √
         </span>
         <p className={`TodoItem-p ${completed && 'TodoItem-p--complete'}`}>
           {text}
         </p>
-        <span onClick={()=>{(onDelete(todo_id))}} className="Icon Icon-delete">
+        <span onClick={onDelete} className="Icon Icon-delete">
           X
         </span>
       </li>
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import {TodoItem} from '../TodoItem';
 
 function TodoList({todos, onComplete, onDelete}) {
-    return todos.map((todo) => <TodoItem key={todo._id} todo={todo} onComplete={onComplete} onDelete={onDelete} />)
+    return todos.map((todo) => (
+      <TodoItem 
+        key={todo._id} 
+        todo={todo} 
+        onComplete={()=>{onComplete(todo._id)}} 
+        onDelete={()=>{onDelete(todo._id)}} 
+      />
+    ))
 }
 
 export {TodoList}
