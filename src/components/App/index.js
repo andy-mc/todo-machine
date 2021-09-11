495b1b688347faaaa31d473838c64b1d7d248839
Andrés Muñoz <andres.munoz@idukay.com>
2021-09-11 12:15:39 -0500

    move methos to app onccomplet and onremove

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -27,13 +27,35 @@ function App() {
     return todoText.includes(searchValueText)
   })
 
+  const onComplete = (todo_id) => {
+    const newTodos = todos.map((todo) => {
+      if(todo._id === todo_id){
+        console.log('todo:', !!!todo.completed)
+        todo.completed = !todo.completed
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  const onDelete = (todo_id) => {
+    const newTodos = todos.filter((todo) => {
+      return todo._id !== todo_id
+    })     
+    setTodos(newTodos)
+  }
+
   return (  
     <Background>
       <div className="container">
         <div className="canvas">
           <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
           <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-          <TodoList todos={filteredTodos} setTodos={setTodos} />
+          <TodoList 
+            todos={filteredTodos} 
+            onComplete={onComplete} 
+            onDelete={onDelete} 
+          />
           <CreateTodo />
         </div>
       </div>
--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -1,26 +1,7 @@
 import React from 'react';
 import {TodoItem} from '../TodoItem';
 
-function TodoList({todos, setTodos}) {
-    const onComplete = (todo_id) => {
-      const newTodos = todos.map((todo) => {
-        if(todo._id === todo_id){
-          console.log('todo:', !!!todo.completed)
-          todo.completed = !todo.completed
-        }
-
-        return todo
-      })
-      setTodos(newTodos)
-    }
-  
-    const onDelete = (todo_id) => {
-      const newTodos = todos.filter((todo) => {
-        return todo._id !== todo_id
-      })     
-      setTodos(newTodos)
-    }
-
+function TodoList({todos, onComplete, onDelete}) {
     return todos.map((todo) => <TodoItem key={todo._id} todo={todo} onComplete={onComplete} onDelete={onDelete} />)
 }
 
